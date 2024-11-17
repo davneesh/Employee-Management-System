2f6513dddd149213ed43a25dc70df4fd03fdcdfd
davneesh <avneeshd09@gmail.com>
2024-11-17 23:06:14 +0530

    Add files

---
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../others/Header'
+
+
+
+const EmployeeDashboard = () => {
+  return (
+    <div className='pd-10'>
+      < Header />
+        
+    </div>
+  )
+}
+
+export default EmployeeDashboard
---
+++ src/components/others/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex items-end justify-between'>
+        <h1>
+            Hello <br>Avneesh </br>
+        </h1>
+      <button> Log out</button>
+    </div>
+  )
+}
+
+export default Header
