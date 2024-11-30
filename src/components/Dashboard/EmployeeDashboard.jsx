10c7129c1bc040f327c74527930e6a8de382f02b
davneesh <avneeshd09@gmail.com>
2024-11-30 23:55:21 +0530

    update files

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from '../others/Header'
+import TaskListNumbers from '../others/TaskListNumber'
+import TaskList from '../TaskList/TaskList'
 
+const EmployeeDashboard = (props) => {
 
-
-const EmployeeDashboard = () => {
   return (
-    <div className='pd-10'>
-      < Header />
+    <div className='p-10 bg-[#1C1C1C] h-screen'>
         
+        <Header changeUser={props.changeUser} data={props.data}/>
+        <TaskListNumbers data={props.data} />
+        <TaskList data={props.data} />
     </div>
   )
 }
